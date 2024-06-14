0d684268a997f265e7a7d46648e5b3dfe2ad3598
drbea <beavoguimaximeakoi@gmail.com>
2024-06-14 16:22:46 -0400

    ajout de frontend au fichier .gitignore

--- authentification/models.py
+++ authentification/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     date_birth = models.DateField(null=True, blank=True)
     is_instructor = models.BooleanField(default=False)
+    # profil_pic = models.ImageField(null=True, blank=True)
 
     groups = models.ManyToManyField(Group, related_name='customuser_set')
     user_permissions = models.ManyToManyField(Permission, related_name='customuser_set_permissions')
--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
+# from config import settings
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('auth/', include('authentification.urls')),
     path('api/', include('api.urls')),
 ]
+
+# if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
